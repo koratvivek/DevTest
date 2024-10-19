059aff290bc3b177e8b1ba33ee7c3a8066cdca40
vivek korat <vivek@viveks-MacBook-Air.local>
2024-10-19 21:03:35 +0530

    Resolved merge conflicts

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -143,14 +143,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv
-(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = os.getenv
-(EMAIL_HOST_PASSWORD)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 context = ssl._create_unverified_context()
-
-# Override the connection creation method
 
 
 class CustomSMTP(smtplib.SMTP):
